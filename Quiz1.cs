f1416826adb16eb145d435840ce8c86d7a8a3210
DenysMTL <47490724+DenysMTL@users.noreply.github.com>
2019-11-05 16:26:46 -0500

    Add files via upload

---
+++ Quiz1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please insert number of rows: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                if (i !=0) {
+                    Console.WriteLine("*");
+                }
+
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
+    
---
+++ Quiz2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+        class SumOfDigits
+        {
+            public static void Main(String[] args)
+            {
+                Console.Write("Enter a number : ");
+                int number = Convert.ToInt16(Console.ReadLine());
+                int sum = 0;
+                while (number != 0)
+                {
+                    sum += number % 10;
+                    number /= 10; 
+                }
+                Console.Write("The result would be : {0} ", sum);
+
+                Console.ReadKey();
+
+            }
+        }
+    }
